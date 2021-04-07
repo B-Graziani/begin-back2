139b482875724537929489cb9b84e320b5ea3113
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-07 11:04:15 +0200

    understand?

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :restaurants, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning restaurants..."
+Restaurant.destroy_all
+
+puts "creating restaurants"
+Restaurant1 = { name: "Mcdonald", rating: "1", address: "223 avenue de clichy"}
+Restaurant2 = { name: "Tour d'argent", rating: "5", address: "1 avenue de charles de gaule"}
+Restaurant3 = { name: "Chez Wung", rating: "3", address: "Paris 13, avenue du dragon"}
+
+Restaurants  = []
+
+[ Restaurant1, Restaurant2, Restaurant3].each_with_index do |attributes, index|
+  restaurant = Restaurant.create!(attributes)
+  Restaurants << restaurant
+end
+
+
+
+
+
